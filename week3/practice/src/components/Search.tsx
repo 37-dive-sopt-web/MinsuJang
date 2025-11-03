aab5a49ba8f612ad30a6c33c3900d7d9e0be65f9
Tnalxmsk <alstnwkd990@naver.com>
2025-11-03 13:55:22 +0900

    :art: style: 서치바 스타일링

--- week3/practice/src/components/Search.tsx
+++ week3/practice/src/components/Search.tsx
@@ -1,4 +1,13 @@
+/** @tsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import * as React from "react";
+
+const searchContainer = css`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+`;
 
 interface SearchProps {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -8,7 +17,7 @@ interface SearchProps {
 const Search = ({ handleChange, onSearch }: SearchProps) => {
 
   return (
-    <div>
+    <div css={searchContainer}>
       <input type="text" onChange={handleChange} />
       <button onClick={onSearch}>검색</button>
     </div>
