b40ab6c2e7aaae6aa4543124b17ec710ca05cbf5
Tnalxmsk <alstnwkd990@naver.com>
2025-11-05 01:39:58 +0900

    :wrench: init: setup emotion css

---
+++ week3/homework/src/styles/color.js
@@ -0,0 +1,30 @@
+export const colors = {
+  primary: {
+    primaryGradient:
+      'linear-gradient(257deg, #ff5125 0%, #ff6624d 50%, #ff8973 100%)',
+    primary700: '#ff4625',
+    primary500: '#ff564d',
+    primary400: '#ff7d76',
+    primary200: '#ffc3b8',
+    primary50: '#fff3ef',
+    primary20: '#FFF4EA',
+  },
+  grayScale: {
+    gray800: '#3d3b3a',
+    gray600: '#605e5d',
+    gray500: '#7f7d7d',
+    gray400: '#a8a4a3',
+    gray200: '#c8c5c4',
+    gray100: '#d8d4d3',
+    gray50: '#f0eae6',
+    gray30: '#f8f4f2',
+  },
+  semantic: {
+    blue: '#4d7dff',
+  },
+  transparent: {
+    black20per: '#1b1b1b20',
+  },
+  white: '#fefcfb',
+  black: '#1b1b1b',
+};
