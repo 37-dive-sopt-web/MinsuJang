2b7fbf0908fea4be004b8d8fb5c7eee39c3991fd
Tnalxmsk <alstnwkd990@naver.com>
2025-11-06 15:33:26 +0900

    :sparkles: feat: 게임 컴포넌트 모달 추가
    게임 성공, 실패 시의 모달 제어 로직 구현

--- week3/homework/src/components/game/Game.jsx
+++ week3/homework/src/components/game/Game.jsx
@@ -5,12 +5,17 @@ import Status from "./Status.jsx";
 import Row from "../common/Row.jsx";
 import { useHandleDeck } from "../../features/game/useHandleDeck.js";
 import { useCallback, useEffect } from "react";
-import { INITIAL_LEVEL, LEVEL_OPTIONS } from "../../const/game.js";
+import { GAME_GRID_SIZE, GAME_PLAY_TIME, INITIAL_LEVEL, LEVEL_OPTIONS } from "../../const/game.js";
 import { useTimer } from "../../features/game/useTimer.js";
 import { useHandleCardGame } from "../../features/game/useHandleCardGame.js";
+import Modal from "../common/Modal.jsx";
+import useModal from "../../features/comon/useModal.js";
+import { useCountdown } from "../../features/game/useCountdown.js";
+import theme from "../../styles/theme.js";
 
 const Game = () => {
   const { deckInfo, generateDeck } = useHandleDeck();
+  const { status, level, data } = deckInfo;
   const {
     completeCards,
     guideMessage,
@@ -20,13 +25,17 @@ const Game = () => {
     isFlipped,
     resetCards,
   } = useHandleCardGame(deckInfo);
-  const { formattedTime, isRunning, startTimer, stopTimer, resetTimer } = useTimer(45, deckInfo.status);
-
-  const { status, level, data } = deckInfo;
+  const { formattedTime, isRunning, startTimer, stopTimer, resetTimer } = useTimer(GAME_PLAY_TIME[level], deckInfo.status);
+  const { isOpen, openModal, closeModal } = useModal();
+  const { countdown } = useCountdown(3, isOpen, () => {
+    closeModal();
+    handleResetGame();
+  });
   const currentLevel = level ?? INITIAL_LEVEL;
-  const cardSize = level === 1 ? 80 : 60;
+  const cardSize = GAME_GRID_SIZE[currentLevel].size;
   const successPairCount = completeCards.size / 2 ?? 0;
   const totalPairCount = data ? data.length / 2 : 0;
+  const isAllComplete = totalPairCount > 0 && successPairCount === totalPairCount;
 
   const handleResetGame = () => {
     resetTimer();
@@ -35,14 +44,13 @@ const Game = () => {
   };
 
   const handleChange = useCallback((e) => {
-    resetTimer();
-    resetCards();
     const next = Number(e.target.value);
+    resetTimer(GAME_PLAY_TIME[next]);
+    resetCards();
     generateDeck(next);
   }, [generateDeck, resetCards, resetTimer]);
 
   const handleFlipWithStart = useCallback((id) => {
-    console.log(isRunning, status);
     if (!isRunning && status === 'ready') {
       startTimer();
     }
@@ -56,50 +64,71 @@ const Game = () => {
   }, [status, generateDeck]);
 
   useEffect(() => {
-    if (totalPairCount > 0 && successPairCount === totalPairCount) {
+    if (isAllComplete) {
       stopTimer();
+      openModal();
     }
-  }, [successPairCount, totalPairCount, stopTimer]);
+  }, [isAllComplete, stopTimer, openModal]);
+
+  useEffect(() => {
+    if (Number(formattedTime) === 0 && !isAllComplete) {
+      stopTimer();
+      openModal();
+    }
+  }, [formattedTime, isAllComplete, stopTimer, openModal]);
 
   return (
-    <Row height={100}>
-      <Row.Item flex={7} height={100}>
-        <Header>
-          <Header.Title title={HEADER_TITLE.GAME} size="h4" />
-          <Header.RightButton label="게임 리셋" onClick={handleResetGame} />
-        </Header>
-        <Deck status={status} level={level} contents={() => (
-          data.map((card) => (
-            <Deck.Card
-              id={card.id}
-              key={card.id}
-              value={card.value}
-              handleFlipCard={handleFlipWithStart}
-              isFlipped={isFlipped}
-              isComplete={isComplete}
-              cardSize={cardSize}
+    <>
+      <Row height={100}>
+        <Row.Item flex={7} height={100}>
+          <Header>
+            <Header.Title title={HEADER_TITLE.GAME} size="h4" />
+            <Header.RightButton label="게임 리셋" onClick={handleResetGame} />
+          </Header>
+          <Deck status={status} level={level} contents={() => (
+            data.map((card) => (
+              <Deck.Card
+                id={card.id}
+                key={card.id}
+                value={card.value}
+                handleFlipCard={handleFlipWithStart}
+                isFlipped={isFlipped}
+                isComplete={isComplete}
+                cardSize={cardSize}
+              />
+            ))
+          )}
+          />
+        </Row.Item>
+        <Row.Item flex={3} padding={20} height={100}>
+          <Status>
+            <Status.Selector
+              level={currentLevel}
+              handleChange={handleChange}
+              options={LEVEL_OPTIONS}
             />
-          ))
-        )}
+            <Status.Info
+              time={formattedTime}
+              successPairCount={successPairCount}
+              totalPairCount={totalPairCount}
+            />
+            <Status.Guide message={guideMessage} />
+            <Status.History histories={history} />
+          </Status>
+        </Row.Item>
+      </Row>
+      <Modal open={isOpen} onClose={closeModal}>
+        <Modal.Title title={isAllComplete ? '축하해요!!!!' : '아쉽네요 ㅠㅠ'} />
+        <Modal.Text
+          text={isAllComplete ? `Level${level}을 ${45 - Number(formattedTime)}0초 만에 클리어했어요` : '모든 카드 짝 맞추기에 실패했어요 ㅜㅜ'} />
+        <Modal.Text
+          text={`${countdown}초 후 자동으로 새 게임을 시작해요`}
+          font={theme.fonts.body2}
+          color={isAllComplete ? 'green' : 'red'}
         />
-      </Row.Item>
-      <Row.Item flex={3} padding={20} height={100}>
-        <Status>
-          <Status.Selector
-            level={currentLevel}
-            handleChange={handleChange}
-            options={LEVEL_OPTIONS}
-          />
-          <Status.Info
-            time={formattedTime}
-            successPairCount={successPairCount}
-            totalPairCount={totalPairCount}
-          />
-          <Status.Guide message={guideMessage} />
-          <Status.History histories={history} />
-        </Status>
-      </Row.Item>
-    </Row>
+      </Modal>
+      <button onClick={openModal}>open</button>
+    </>
   );
 };
 
