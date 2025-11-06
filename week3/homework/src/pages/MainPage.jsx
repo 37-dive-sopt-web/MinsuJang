11b6d4f7a0f75da405818647eb5c0d29e4c34e38
Tnalxmsk <alstnwkd990@naver.com>
2025-11-06 15:30:47 +0900

    :truck: rename: 디렉토리 및 경로 변경
    features/common 디렉토리로 이동

--- week3/homework/src/pages/MainPage.jsx
+++ week3/homework/src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import RootLayout from "../app/layouts/RootLayout.jsx";
 import { TAB, TAB_LABEL, tabList } from "../const/tab.js";
 import Header from "../components/common/Header.jsx";
-import { useHandleTab } from "../features/useHandleTab.js";
+import { useHandleTab } from "../features/comon/useHandleTab.js";
 import Game from "../components/game/Game.jsx";
 import Rank from "../components/rank/Rank.jsx";
 import styled from "@emotion/styled";
