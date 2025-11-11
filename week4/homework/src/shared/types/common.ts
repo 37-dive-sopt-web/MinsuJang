b139b3f4fd543ebda21c58804882ca1ee9f1863f
Tnalxmsk <alstnwkd990@naver.com>
2025-11-12 00:29:54 +0900

    :sparkles: feat: RecipeVariants를 기반으로 PartialVariants 타입 유틸 정의

--- week4/homework/src/shared/types/common.ts
+++ week4/homework/src/shared/types/common.ts
@@ -1,5 +1,8 @@
 import * as React from 'react';
+import type { RecipeVariants, RuntimeFn } from '@vanilla-extract/recipes';
 
 export interface ChildrenProps {
   children: React.ReactNode;
 }
+
+export type PartialVariants<T extends RuntimeFn<Record<string, any>>> = Partial<RecipeVariants<T>>;
