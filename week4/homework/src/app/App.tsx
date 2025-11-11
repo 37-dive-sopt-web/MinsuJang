3d2df5d9cfefdbb4a1688b6bffe9b28bf5e35b14
Tnalxmsk <alstnwkd990@naver.com>
2025-11-11 17:16:37 +0900

    :sparkles: feat: ThemeProvider 구현 및 적용

--- week4/homework/src/app/App.tsx
+++ week4/homework/src/app/App.tsx
@@ -1,13 +1,14 @@
 import { AppRouterProvider } from '@app/providers/AppRouterProvider.tsx';
 import { AppQueryProvider } from '@app/providers/AppQueryProvider.tsx';
+import { ThemeProvider } from '@app/providers/ThemeProvider.tsx';
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <AppQueryProvider>
         <AppRouterProvider />
       </AppQueryProvider>
-    </>
+    </ThemeProvider>
   );
 }
 
