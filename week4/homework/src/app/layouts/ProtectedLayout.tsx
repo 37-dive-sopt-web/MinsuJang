0e13dc3d8025769f56cc97c89449c0276b2f72dc
Tnalxmsk <alstnwkd990@naver.com>
2025-11-18 06:47:12 +0900

    :sparkles: feat: 보안 관련 설정
    - zustand persist를 활용하여 로그인 상태 저장 및 로그인되지 않은 사용자는 접근할수 없도록 구현

--- week4/homework/src/app/layouts/ProtectedLayout.tsx
+++ week4/homework/src/app/layouts/ProtectedLayout.tsx
@@ -1,8 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { protectedLayoutWrapper } from '@app/layouts/Layout.css.ts';
 import AppHeader from '@widgets/app-header/ui/AppHeader.tsx';
+import { useAuthStore } from '@shared/model/useAuthStore.ts';
 
 const ProtectedLayout = () => {
+  const { isLoggedIn } = useAuthStore();
+
+  if (!isLoggedIn) {
+    window.location.href = '/auth/login';
+  }
+
   return (
     <>
       <AppHeader />
