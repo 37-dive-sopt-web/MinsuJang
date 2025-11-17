bca7abc60ce6e741baa4d5d37989fd491a6867ce
Tnalxmsk <alstnwkd990@naver.com>
2025-11-17 21:49:04 +0900

    :sparkles: feat: FontType 추가
    정의해둔 fonts의 key 기반 타입 구현

--- week4/homework/src/shared/types/common.ts
+++ week4/homework/src/shared/types/common.ts
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import type { RecipeVariants, RuntimeFn } from '@vanilla-extract/recipes';
+import type { fonts } from '@shared/styles/fonts.css.ts';
 
 export interface ChildrenProps {
   children: React.ReactNode;
 }
 
 export type PartialVariants<T extends RuntimeFn<Record<string, any>>> = Partial<RecipeVariants<T>>;
+
+export type FontKey = keyof typeof fonts;
