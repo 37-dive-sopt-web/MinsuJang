3b16aa226ba789a116373899551fa15e986a538a
Tnalxmsk <alstnwkd990@naver.com>
2025-11-18 01:13:15 +0900

    :wrench: chore: overlay-kit 의존성 추가 및 프로바이더 적용
    선언적으로 Modal UI를 관리하고, 모달 상태 관리의 용이함을 위해 적용

--- week4/homework/src/app/App.tsx
+++ week4/homework/src/app/App.tsx
@@ -1,13 +1,16 @@
 import { AppRouterProvider } from '@app/providers/AppRouterProvider.tsx';
 import { AppQueryProvider } from '@app/providers/AppQueryProvider.tsx';
 import { ThemeProvider } from '@app/providers/ThemeProvider.tsx';
+import { OverlayProvider } from 'overlay-kit';
 
 function App() {
   return (
     <ThemeProvider>
-      <AppQueryProvider>
-        <AppRouterProvider />
-      </AppQueryProvider>
+      <OverlayProvider>
+        <AppQueryProvider>
+          <AppRouterProvider />
+        </AppQueryProvider>
+      </OverlayProvider>
     </ThemeProvider>
   );
 }
