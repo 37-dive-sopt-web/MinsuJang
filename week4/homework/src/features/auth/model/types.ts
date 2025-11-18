4c3b8ff0490fa68e86a9fa59d0b7d574643a8f53
Tnalxmsk <alstnwkd990@naver.com>
2025-11-18 17:28:53 +0900

    :sparkles: feat: 회원가입 퍼널 스탭 공통 props type 추가

--- week4/homework/src/features/auth/model/types.ts
+++ week4/homework/src/features/auth/model/types.ts
@@ -1,1 +1,9 @@
+import type { FieldErrors, UseFormRegister } from 'react-hook-form';
+import type { SignUpFormValues } from '@features/auth/model/authSchema.ts';
+
 export type SignUpFunnelStep = 'ID' | 'PASSWORD' | 'INFO';
+
+export type SignUpFunnelStepProps = {
+  register: UseFormRegister<SignUpFormValues>;
+  errors: FieldErrors<SignUpFormValues>;
+};
