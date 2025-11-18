81683e9d093b07a87e750f2540bccc03338b9f55
Tnalxmsk <alstnwkd990@naver.com>
2025-11-18 17:29:33 +0900

    :sparkles: feat: 회원가입 mutation 및 RHF 커스텀 훅 구현

--- week4/homework/src/features/users/model/useGetUserInfo.ts
+++ week4/homework/src/features/users/model/useGetUserInfo.ts
@@ -5,6 +5,10 @@ import { isAppError } from '@shared/apis/error.ts';
 export const useGetUserInfo = (userId: string) => {
   return useSuspenseQuery({
     ...userQueriesKey.users.byId(userId),
+    staleTime: 0,
+    refetchOnMount: 'always',
+    refetchOnWindowFocus: 'always',
+    refetchOnReconnect: 'always',
     retry: (failureCount, error) => {
       if (isAppError(error) && error.code === 'COMMON-404') return false;
       return failureCount < 1;
