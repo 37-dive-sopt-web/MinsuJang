f6a19c89693e51ce9c0b143078bfd88ce23d225a
Tnalxmsk <alstnwkd990@naver.com>
2025-11-18 17:27:58 +0900

    :sparkles: feat: 회원가입, 회원정 보 수정 fetch 함수 구현

--- week4/homework/src/entities/users/api/users.ts
+++ week4/homework/src/entities/users/api/users.ts
@@ -1,9 +1,18 @@
 import { kyInstance } from '@shared/apis/kyInstance.ts';
 import type { BaseResponse } from '@shared/types/http.ts';
-import type { LoginResponse, UserInfoResponse } from '@entities/users/model/response.ts';
-import type { LoginRequest } from '@entities/users/model/request.ts';
+import type {
+  LoginResponse,
+  SignUpResponse,
+  UpdateUserInfoResponse,
+  UserInfoResponse,
+} from '@entities/users/model/response.ts';
+import type {
+  LoginRequest,
+  SignUpRequest,
+  UpdateUserRequest,
+} from '@entities/users/model/request.ts';
 
-export const fetchUserById = async (id: string = '1') => {
+export const fetchUserById = async (id: string) => {
   return await kyInstance.get(`users/${id}`).json<BaseResponse<UserInfoResponse>>();
 };
 
@@ -15,5 +24,19 @@ export const login = async (requestBody: LoginRequest) => {
   return await kyInstance
     .post('auth/login', { json: requestBody })
     .json<BaseResponse<LoginResponse>>()
+    .then((res) => res.data);
+};
+
+export const signup = async (requestBody: SignUpRequest) => {
+  return kyInstance
+    .post('users', { json: requestBody })
+    .json<BaseResponse<SignUpResponse>>()
+    .then((res) => res.data);
+};
+
+export const updateUser = async (id: string, requestBody: UpdateUserRequest) => {
+  return kyInstance
+    .patch(`users/${id}`, { json: requestBody })
+    .json<BaseResponse<UpdateUserInfoResponse>>()
     .then((res) => res.data);
 };
